9aa09fea50c317da50524109e5c69ed551c1d1d9
Logan Cerkovnik <LCERKOVNIK@davita.corp>
2018-07-19 12:49:54 -0600

    fixed example

--- main.py
+++ main.py
@@ -11,12 +11,33 @@ from flask import Flask
 server = Flask(__name__)
 app = dash.Dash(__name__, server=server)
 app.scripts.config.serve_locally = True
-app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
+# app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
-app.layout = html.Div(...)
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
 
-@app.callback(...)
-def update_figure():
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5],
+                	'type': 'bar', 'name': u'Montréal'},
+            ],
+            'layout': {
+                'title': 'Dash Data Visualization'
+            }
+        }
+    )
+])
+
+# app.layout = html.Div(...)
+# @app.callback(...)
+# def update_figure():
 
 
 if __name__ == '__main__':
